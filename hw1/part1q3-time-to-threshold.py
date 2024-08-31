8badca87e73e483cfc8245a1a3db29d1888dbf30
Rotem Hen <rhen@salesforce.com>
2024-08-31 15:50:11 +0300

    reduce scale

--- hw1/part1q3-time-to-threshold.py
+++ hw1/part1q3-time-to-threshold.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 
 
 # Model Parameters
-T       = 10    # Simulation time          [mSec]
+T       = 1.5    # Simulation time          [mSec]
 dt      = 0.1   # Simulation time interval [mSec]
 t_init  = 0     # Stimulus init time       [V]
 vRest   = -70   # Resting potential        [mV]
@@ -20,8 +19,6 @@ time    = np.arange(0, T*1e-3 + dt*1e-3, dt*1e-3)  # Time array
 Vm      = np.ones(len(time))*vRest*1e-3            # Membrane voltage array
 tau_m   = Rm*1e3 * Cm*1e-6                         # Time constant
 spikes  = []                                       # Spikes timings
-
-stim = np.full(len(time), I*1e-3) # Triangular stimulation pattern
 
 # Simulation
 for i, t in enumerate(time[:-1]):
@@ -45,15 +42,15 @@ plt.title('Leaky Integrate-and-Fire Model', fontsize=15)
 plt.ylabel('Membrane Potential (mV)', fontsize=15)
 plt.xlabel('Time (msec)', fontsize=15)
 plt.plot(time*1e3, Vm*1e3, linewidth=5, label = 'Vm')
-plt.plot(time*1e3, np.full(len(time), 80), label = 'Stimuli (Scaled)', color='sandybrown', linewidth=2)
+plt.plot(time*1e3, np.full(len(time), 80), label = 'Stimuli', color='sandybrown', linewidth=2)
 plt.ylim([-75, 100])
 plt.axvline(x=spikes[0], c='red', label = 'Spike')
 for s in spikes[1:]:
     plt.axvline(x=s, c='red')
 plt.axhline(y=vTh, c='black', label = 'Threshold', linestyle='--')
 
-# Increase the number of scale marks on the x-axis
-x_ticks = np.arange(0, 10, 0.5)  # Adjust the step size to your preference
+# Increase the number of scale marks
+x_ticks = np.arange(0, 1.5, 0.1)
 plt.xticks(x_ticks)
 
 plt.legend()
